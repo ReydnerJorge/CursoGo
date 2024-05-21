d8068c9dbf79f1a804f61f6c3dbb96560de35606
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2024-05-21 20:00:11 -0300

    Update Hello World

---
+++ src/hello/hello.go
@@ -0,0 +1,26 @@
+package main // pacote principal
+
+import "fmt" // import print
+
+func main() { // função principal
+	fmt.Println("Hello World!") // print
+	fmt.Println("Me livrei da maldição hehe")
+	fmt.Print("Continuação dos testes")
+	fmt.Printf("Testes")
+}
+
+/*
+go build hello.go - Buildar o arquivo go
+go run hello.go - executar o arquivo go
+go doc - acessar a documentação do go no cmd
+*/
+
+/*
+"Tente uma, duas, três vezes se possivel tente a
+quarta, a quinta e quantas vezes for necessário. Só
+não desista nas primeiras tentativas, a persisteência é amiga
+da conquista. Se você quer chega aonde a maioria não chega,
+faça o que a maioria não faz."
+
+- Bill Gates
+*/
