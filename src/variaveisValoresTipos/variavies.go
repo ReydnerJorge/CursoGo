2d6276164d3219a88be3411afee6ff3cca0a758d
Reydner96 {Reyd} <reydneroliveirajorge96@gmail.com>
2024-05-27 10:41:33 -0300

    Update teste

--- src/variaveisValoresTipos/variavies.go
+++ src/variaveisValoresTipos/variavies.go
@@ -5,11 +5,13 @@ import (
 )
 
 func main() {
-	x := 10 //operador de declaração
+	x := 13 //operador de declaração
 	y := "Olá boa tarde"
+	z := 17
 
 	fmt.Printf("x: %v, %T\n", x, x)
 	fmt.Printf("y: %v, %T\n\n", y, y)
+	fmt.Printf("z: %v, %T\n", z, z)
 
 	x = 20 // operador de atribuição
 	fmt.Printf("x: %v, %T\n", x, x)
